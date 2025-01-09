704cbda2b6678e252c3ac19f9caec928de3862d4
Cakjon <wicaksanaadisaputro@gmail.com>
2025-01-09 17:17:22 +0700

    update itroduction

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -12,19 +12,21 @@ export default function Introduction() {
             </h2>
             <br />
             <p className="text-white max-w-lg">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil
-              perferendis temporibus impedit, esse tempore incidunt suscipit
-              voluptatum sapiente, in laborum culpa doloremque, vel fugiat
-              possimus officia est similique id enim!
+              Saya Wicaksana Adi Saputro mahasiswa Teknik Informatika semester 8
+              Sekolah Tinggi Teknologi Indonesia Tanjungpinang, mahasiswa aktif
+              dan berprestsai dalam bidang non akademik mulai Tingkat Provinsi
+              sampai dengan intenasional. Mampu berkomunikasi dengan baik dan
+              dapat bekerja sama dalam tim. Memiliki pengalaman kampus merdeka
+              batch 6 dan batch 7
             </p>
           </div>
           <div className="relative w-96 h-96">
-            <Image 
-                src={"/img/wics.jpg"}
-                alt="Wicaksana Adi Saputro"
-                fill={true}
-                priority={true}
-                className="rounded-5xl object-cover"
+            <Image
+              src={"/img/wics.jpg"}
+              alt="Wicaksana Adi Saputro"
+              fill={true}
+              priority={true}
+              className="rounded-5xl object-cover"
             />
           </div>
         </div>
--- src/components/Techs.tsx
+++ src/components/Techs.tsx
@@ -1,30 +1,17 @@
-// export default function Techs() {
-//   return (
-//     <>
-//       <div className="bg-slate-800 w-screen min-h-[300px]">
-//         <div className="w-32 border-2 mb-8 ml-5" />
-//         <div className="flex justify-center items-center w-full flex-wrap">
-//             <TechCard imageURL="" techStack=""/>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
 import TechCard from "./TechCard";
 
 export default function Tech() {
-    return(
-        <>
-        <div className="bg-slate-800 w-screen min-h-[300px] p-8">
-            <h1 className="text-4xl text-white font-bold mb-4">TECHS</h1>
-            <div className="w-32 border-2 mb-8"/>
-                <div className="flex justify-center items-center w-full flex-wrap">
-                    <TechCard imageURL="/img/js.png" alt="javascript" techStack="C++"/>
-                    <TechCard imageURL="/img/react.png" alt="python" techStack="CSS"/>
-                    <TechCard imageURL="/img/ts.png" alt="typescript" techStack="HTML"/>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="bg-slate-800 w-screen min-h-[300px] p-8">
+        <h1 className="text-4xl text-white font-bold mb-4">TECHS</h1>
+        <div className="w-32 border-2 mb-8" />
+        <div className="flex justify-center items-center w-full flex-wrap">
+          <TechCard imageURL="/img/js.png" alt="javascript" techStack="C++" />
+          <TechCard imageURL="/img/react.png" alt="python" techStack="CSS" />
+          <TechCard imageURL="/img/ts.png" alt="typescript" techStack="HTML" />
+        </div>
+      </div>
+    </>
+  );
 }
