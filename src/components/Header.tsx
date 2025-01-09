6f0d3efe0fd7c594d645320e7940af7175fba745
Cakjon <wicaksanaadisaputro@gmail.com>
2025-01-09 17:19:35 +0700

    update Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <>
       <div className="w-screen p-8 bg-slate-800 flex justify-between items-center">
-        <h1 className="font-bold text-white text-2xl">Infinite Learning</h1>
+        <h1 className="font-bold text-white text-2xl">Next Js Porto</h1>
       <div className="">
         <ul className="flex">
             <li className="text-white text-lg m-5"><Link href="">Name</Link></li>
