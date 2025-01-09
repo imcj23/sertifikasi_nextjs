8b81762e91794c17a202fe8ddd11dad74be0ad2a
Cakjon <wicaksanaadisaputro@gmail.com>
2025-01-09 17:35:00 +0700

    update techs

--- src/components/Techs.tsx
+++ src/components/Techs.tsx
@@ -7,9 +7,9 @@ export default function Tech() {
         <h1 className="text-4xl text-white font-bold mb-4">TECHS</h1>
         <div className="w-32 border-2 mb-8" />
         <div className="flex justify-center items-center w-full flex-wrap">
-          <TechCard imageURL="/img/js.png" alt="javascript" techStack="js" />
-          <TechCard imageURL="/img/react.png" alt="React" techStack="react" />
-          <TechCard imageURL="/img/ts.png" alt="typescript" techStack="ts" />
+          <TechCard imageURL="/Img/js.png" alt="javascript" techStack="js" />
+          <TechCard imageURL="/Img/react.png" alt="React" techStack="react" />
+          <TechCard imageURL="/Img/ts.png" alt="typescript" techStack="ts" />
         </div>
       </div>
     </>
